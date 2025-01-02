9c55fc273f8bd765dd38351bb4b6fa3256045c9d
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-01-02 15:00:18 +0500

    1.making the language controller app

--- lib/Language_tanslator.dart
+++ lib/Language_tanslator.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:language_translator/View_model/LanguageController.dart';
 
 
 class LanguageTranslator extends StatefulWidget {
@@ -9,6 +11,8 @@ class LanguageTranslator extends StatefulWidget {
 }
 
 class _LanguageTranslatorState extends State<LanguageTranslator> {
+
+  final LanguageController controller = Get.put(LanguageController());
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +25,51 @@ class _LanguageTranslatorState extends State<LanguageTranslator> {
         centerTitle: true,
         backgroundColor: Color(0xff10223d),
         elevation: 0,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: height * .5,),
+              Row(
+                children: [
+                  DropdownButton(
+                    focusColor: Colors.white,
+                      iconDisabledColor: Colors.white,
+                      iconEnabledColor: Colors.white,
+                      hint: Text(controller.originLanguage,style: TextStyle(color: Colors.white),),
+                      items: controller.languages.map((String dropDownStringItem){
+                        return DropdownMenuItem(child: Text(dropDownStringItem),value: dropDownStringItem,);
+                      }).toList(),
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      dropdownColor: Colors.white,
+                    onChanged: (String? value) {
+                      controller.originLanguage = value!;
+                    },
+                      ),
+                  SizedBox(width: width * 0.4,),
+                  Icon(Icons.arrow_right_alt_rounded,color: Colors.white,),
+                  DropdownButton(
+                    focusColor: Colors.white,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                    hint: Text(controller.destinationLanguage,style: TextStyle(color: Colors.white),),
+                    items: controller.languages.map((String dropDownStringItem){
+                      return DropdownMenuItem(child: Text(dropDownStringItem),value: dropDownStringItem,);
+                    }).toList(),
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    dropdownColor: Colors.white,
+                    onChanged: (String? value) {
+                      controller.destinationLanguage = value!;
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: height * .5,),
+
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/View_model/LanguageController.dart
+++ lib/View_model/LanguageController.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 
 class LanguageController extends  GetxController {
 
-
-
-
+   var originLanguage = 'From';
+  var destinationLanguage = 'To';
+  final List<String> languages = ['Urdi','English','Hindi'];
 }
